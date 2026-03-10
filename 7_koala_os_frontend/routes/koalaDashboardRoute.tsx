49c0a871b3b0a1a6055cd241ad6f2a40548a59ad
Life++PoCC <poccahin@gmail.com>
2026-03-10 21:09:51 +0800

    feat(koala-os): add unified dashboard entry route

---
+++ 7_koala_os_frontend/routes/koalaDashboardRoute.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import KoalaOSDashboard from '../components/KoalaOS_Dashboard';
+
+export const koalaDashboardRoute = {
+  path: '/',
+  element: <KoalaOSDashboard />,
+};
